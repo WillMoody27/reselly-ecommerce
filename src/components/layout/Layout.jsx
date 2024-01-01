efef9bff246a397c00e8a78db6cd8d45b891881d
William <whellems@msudenver.edu>
2024-01-01 03:59:59 -0700

    eslint correction for deployment

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Hero from "./Hero";
 import Category from "./Category";
-import Navigation from "./Navigation";
 import ProductSection from "./ProductSection";
 import Copy from "./copy/Copy";
 
